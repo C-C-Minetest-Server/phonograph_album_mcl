2b80d5076b42d1b9162e13054b116b21e3b7a515
1F616EMO <root@1f616emo.xyz>
2026-01-20 18:15:38 +0800

    Stereo for Ambiences and some jukebox musics

--- src/ambience.lua
+++ src/ambience.lua
@@ -15,147 +15,58 @@ local album = phonograph.register_album("phonograph_album_mcl:album_ambience", {
 
 local MP = core.get_modpath("phonograph_album_mcl")
 local function songpath(name)
-    return table.concat({MP, "phonographs", name .. ".ogg"}, DIR_DELIM)
+    return table.concat({ MP, "phonographs", "phonograph_album_mcl_ambience_" .. name .. ".ogg" }, DIR_DELIM)
 end
 
--- Track license: CC BY-SA 4.0
--- Artist page: https://soundcloud.com/dark-reaven-music
-album:register_song("horizonchris96-traitor", {
-    title = S("Traitor"),
-    short_description = S("Played in the nether"),
-    long_description = nil,
-    artist = "Dark Reaven Music",
-    filepath = songpath("phonograph_album_mcl_ambience_horizonchris96-traitor"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
+--[[
+    Dark Reaven Music: https://soundcloud.com/dark-reaven-music
+    Jester: https://www.youtube.com/@Jester-8-bit
+    Exhale & Tim Unwin: https://www.youtube.com/channel/UClFo_JDWoG4NGrPQY0JPD_g
+]]
 
--- Track license: CC BY-SA 4.0
--- Original: mcl menu theme
--- Artist page: https://soundcloud.com/dark-reaven-music
-album:register_song("drm_theme", {
-    title = S("Calmed Cube"),
-    short_description = S("The song played in the main menu"),
-    long_description = nil,
-    artist = "Dark Reaven Music",
-    filepath = songpath("phonograph_album_mcl_ambience_drm_theme"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
-
--- Track license: CC BY-SA 4.0
--- Artist page: https://www.youtube.com/@Jester-8-bit
-album:register_song("Jester-Hailing_Forest", {
-    title = S("Hailing Forest"),
-    short_description = S("Played in the overworld"),
-    long_description = nil,
-    artist = "Jester",
-    filepath = songpath("phonograph_album_mcl_ambience_Jester-Hailing_Forest"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
-
--- Track license: CC BY-SA 4.0
--- Artist page: https://www.youtube.com/@Jester-8-bit
-album:register_song("Jester-Gift", {
-    title = S("Gift"),
-    short_description = S("Played in the overworld"),
-    long_description = nil,
-    artist = "Jester",
-    filepath = songpath("phonograph_album_mcl_ambience_Jester-Gift"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
-
--- Track license: CC BY-SA 4.0
--- Artist page: https://www.youtube.com/@Jester-8-bit
-album:register_song("Jester-0dd-BL0ck", {
-    title = S("0dd BL0ck"),
-    short_description = S("Played while mining"),
-    long_description = nil,
-    artist = "Jester",
-    filepath = songpath("phonograph_album_mcl_ambience_Jester-0dd-BL0ck"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
-
--- Track license: CC BY-SA 4.0
--- Artist page: https://www.youtube.com/@Jester-8-bit
-album:register_song("Jester-Flock-of-One", {
-    title = S("Flock of One"),
-    short_description = S("Played in the overworld"),
-    long_description = nil,
-    artist = "Jester",
-    filepath = songpath("phonograph_album_mcl_ambience_Jester-Flock-of-One"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
-
--- Track license: CC BY-SA 4.0
--- Artist page: https://www.youtube.com/channel/UClFo_JDWoG4NGrPQY0JPD_g
-album:register_song("exhale_and_tim_unwin-valley_of_ghosts", {
-    title = S("Valley of Ghosts"),
-    short_description = S("Played in the nether"),
-    long_description = nil,
-    artist = "Exhale & Tim Unwin",
-    filepath = songpath("phonograph_album_mcl_ambience_exhale_and_tim_unwin-valley_of_ghosts"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
-
--- Track license: CC BY-SA 4.0
--- Artist page: https://www.youtube.com/channel/UClFo_JDWoG4NGrPQY0JPD_g
-album:register_song("exhale_and_tim_unwin-lonely_blossom", {
-    title = S("Lonely Blossom"),
-    short_description = S("Played in the overworld"),
-    long_description = nil,
-    artist = "Exhale & Tim Unwin",
-    filepath = songpath("phonograph_album_mcl_ambience_exhale_and_tim_unwin-lonely_blossom"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
-
--- Track license: CC BY-SA 4.0
--- Artist page: https://www.youtube.com/channel/UClFo_JDWoG4NGrPQY0JPD_g
-album:register_song("exhale_and_tim_unwin-farmer", {
-    title = S("Farmer"),
-    short_description = S("Played in the overworld"),
-    long_description = nil,
-    artist = "Exhale & Tim Unwin",
-    filepath = songpath("phonograph_album_mcl_ambience_exhale_and_tim_unwin-farmer"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
-
--- Track license: CC BY-SA 4.0
-album:register_song("diminixed-pianowtune02", {
-    title = S("Afternoon Lullaby"),
-    short_description = S("Played in the overworld"),
-    long_description = nil,
-    artist = "Diminixed",
-    filepath = songpath("phonograph_album_mcl_ambience_diminixed-pianowtune02"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
-
--- Track license: CC BY-SA 4.0
-album:register_song("diminixed-ambientwip02", {
-    title = S("Spooled"),
-    short_description = S("Played in the end"),
-    long_description = nil,
-    artist = "Diminixed",
-    filepath = songpath("phonograph_album_mcl_ambience_diminixed-ambientwip02"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
-
--- Track license: CC BY-SA 4.0
-album:register_song("diminixed-nevergrowup04", {
-    title = S("Never Grow Up"),
-    short_description = S("Played in the overworld"),
-    long_description = nil,
-    artist = "Diminixed",
-    filepath = songpath("phonograph_album_mcl_ambience_diminixed-nevergrowup04"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA4,
-})
+for _, data in ipairs({
+    { "horizonchris96-traitor", S("Traitor"),
+        S("Played in the nether"), "Dark Reaven Music", phonograph.licenses.CCBYSA4 },
+    { "drm_theme", S("Calmed Cube"),
+        S("The song played in the main menu"), "Dark Reaven Music", phonograph.licenses.CCBYSA4 },
+    { "Jester-Hailing_Forest", S("Hailing Forest"),
+        S("Played in the overworld"), "Jester", phonograph.licenses.CCBYSA4 },
+    { "Jester-Gift", S("Gift"),
+        S("Played in the overworld"), "Jester", phonograph.licenses.CCBYSA4 },
+    { "Jester-0dd-BL0ck", S("0dd BL0ck"),
+        S("Played while mining"), "Jester", phonograph.licenses.CCBYSA4 },
+    { "Jester-Flock-of-One", S("Flock of One"),
+        S("Played in the overworld"), "Jester", phonograph.licenses.CCBYSA4 },
+    { "exhale_and_tim_unwin-valley_of_ghosts", S("Valley of Ghosts"),
+        S("Played in the nether"), "Exhale & Tim Unwin", phonograph.licenses.CCBYSA4 },
+    { "exhale_and_tim_unwin-lonely_blossom", S("Lonely Blossom"),
+        S("Played in the overworld"), "Exhale & Tim Unwin", phonograph.licenses.CCBYSA4 },
+    { "exhale_and_tim_unwin-farmer", S("Farmer"),
+        S("Played in the overworld"), "Exhale & Tim Unwin", phonograph.licenses.CCBYSA4 },
+    { "diminixed-pianowtune02", S("Afternoon Lullaby"),
+        S("Played in the overworld"), "Diminixed", phonograph.licenses.CCBYSA4 },
+    { "diminixed-ambientwip02", S("Spooled"),
+        S("Played in the end"), "Diminixed", phonograph.licenses.CCBYSA4 },
+    { "diminixed-nevergrowup04", S("Never Grow Up"),
+        S("Played in the overworld"), "Diminixed", phonograph.licenses.CCBYSA4 },
+}) do
+    local id, title, short_description, artist, license = unpack(data)
+    album:register_song(id, {
+        title = title,
+        short_description = short_description,
+        long_description = nil,
+        artist = artist,
+        license = license,
+        spec = {
+            filepath = songpath(id),
+        },
+        multichannel_specs = {
+            {
+                filepath = songpath(id .. "_ch0"),
+            },
+            {
+                filepath = songpath(id .. "_ch1"),
+            },
+        },
+    })
+end
--- src/jukebox.lua
+++ src/jukebox.lua
@@ -14,95 +14,44 @@ local album = phonograph.register_album("phonograph_album_mcl:album_jukebox", {
 
 local MP = core.get_modpath("phonograph_album_mcl")
 local function songpath(name)
-    return table.concat({MP, "phonographs", name .. ".ogg"}, DIR_DELIM)
+    return table.concat({ MP, "phonographs", "phonograph_album_mcl_mcl_jukebox_" .. name .. ".ogg" }, DIR_DELIM)
 end
 
--- Track license: CC0
-album:register_song("track_1", {
-    title = S("The Evil Sister"),
-    short_description = nil,
-    long_description = nil,
-    artist = S("@1 (Jordach's Mix)", "SoundHelix"),
-    filepath = songpath("phonograph_album_mcl_mcl_jukebox_track_1"),
-    spec = {},
-    license = phonograph.licenses.CC0,
-})
-
--- Track license: CC0
-album:register_song("track_2", {
-    title = S("The Energetic Rat"),
-    short_description = nil,
-    long_description = nil,
-    artist = S("@1 (Jordach's Mix)", "SoundHelix"),
-    filepath = songpath("phonograph_album_mcl_mcl_jukebox_track_2"),
-    spec = {},
-    license = phonograph.licenses.CC0,
-})
-
--- Track license: CC0
-album:register_song("track_3", {
-    title = S("Eastern Feeling"),
-    short_description = nil,
-    long_description = nil,
-    artist = "Jordach",
-    filepath = songpath("phonograph_album_mcl_mcl_jukebox_track_3"),
-    spec = {},
-    license = phonograph.licenses.CC0,
-})
-
--- Track license: CC0
-album:register_song("track_4", {
-    title = S("Minetest"),
-    short_description = nil,
-    long_description = nil,
-    artist = "Jordach",
-    filepath = songpath("phonograph_album_mcl_mcl_jukebox_track_4"),
-    spec = {},
-    license = phonograph.licenses.CC0,
-})
-
--- Track license: CC BY 3.0
--- Source: https://ccmixter.org/files/mactonite/65379
-album:register_song("track_5", {
-    title = S("Soaring over the sea"),
-    short_description = nil,
-    long_description = nil,
-    artist = "Darkroom", -- or mactonite
-    filepath = songpath("phonograph_album_mcl_mcl_jukebox_track_5"),
-    spec = {},
-    license = phonograph.licenses.CCBY3,
-})
-
--- Track license: CC BY-SA 3.0
--- Source: https://opengameart.org/content/winter-feeling
-album:register_song("track_6", {
-    title = S("Winter Feeling"),
-    short_description = nil,
-    long_description = nil,
-    artist = "Tom Peter",
-    filepath = songpath("phonograph_album_mcl_mcl_jukebox_track_6"),
-    spec = {},
-    license = phonograph.licenses.CCBYSA3,
-})
-
--- Track license: CC0
-album:register_song("track_7", {
-    title = S("Synthgroove"),
-    short_description = nil,
-    long_description = nil,
-    artist = S("@1 (Jordach's Mix)", "HeroOfTheWinds"),
-    filepath = songpath("phonograph_album_mcl_mcl_jukebox_track_7"),
-    spec = {},
-    license = phonograph.licenses.CC0,
-})
-
--- Track license: CC0
-album:register_song("track_8", {
-    title = S("The Clueless Frog"),
-    short_description = nil,
-    long_description = nil,
-    artist = S("@1 (Jordach's Mix)", "SoundHelix"),
-    filepath = songpath("phonograph_album_mcl_mcl_jukebox_track_8"),
-    spec = {},
-    license = phonograph.licenses.CC0,
-})
+for _, data in ipairs({
+    { "track_1", S("The Evil Sister"), false,
+        S("@1 (Jordach's Mix)", "SoundHelix"), phonograph.licenses.CC0 },
+    { "track_2", S("The Energetic Rat"), false,
+        S("@1 (Jordach's Mix)", "SoundHelix"), phonograph.licenses.CC0 },
+    { "track_3", S("Eastern Feeling"), false,
+        "Jordach", phonograph.licenses.CC0 },
+    { "track_4", S("Minetest"), false,
+        "Jordach", phonograph.licenses.CC0 },
+    { "track_5", S("Soaring over the sea"), true, -- https://ccmixter.org/files/mactonite/65379
+        "Darkroom", phonograph.licenses.CCBY3 },
+    { "track_6", S("Winter Feeling"), true,       -- https://opengameart.org/content/winter-feeling
+        "Tom Peter", phonograph.licenses.CCBYSA3 },
+    { "track_7", S("Synthgroove"), false,
+        S("@1 (Jordach's Mix)", "HeroOfTheWinds"), phonograph.licenses.CC0 },
+    { "track_8", S("The Clueless Frog"), false,
+        S("@1 (Jordach's Mix)", "SoundHelix"), phonograph.licenses.CC0 },
+}) do
+    local id, title, has_stereo, artist, license = unpack(data)
+    album:register_song(id, {
+        title = title,
+        short_description = short_description,
+        long_description = nil,
+        artist = artist,
+        license = license,
+        spec = {
+            filepath = songpath(id),
+        },
+        multichannel_specs = has_stereo and {
+            {
+                filepath = songpath(id .. "_ch0"),
+            },
+            {
+                filepath = songpath(id .. "_ch1"),
+            },
+        } or nil,
+    })
+end
